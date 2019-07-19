3fb96f38fbd7a23aeaf1b07ff6f41d8864e9a297
luis <48030756+luis77e@users.noreply.github.com>
2019-07-19 14:48:21 -0300

    closes#2
    Formulário de cadastro de novo Usuário

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     </section>
 
     <!-- Formulario de Login -->
-    <section class="row">
+    <section class="row mb-5" >
         <div class="col-lg-4 offset-lg-4 bg-light rounded" id="caixaLogin">
             <h2 class="text-center mt-2">Entrada no Sistema</h2>
             <form action="#" id="formLogin" class="p-2">
@@ -67,7 +67,71 @@
             </form>
         </div>
     </section>
+<!-- Foemulario de Cadastro -->
+        <section class="row mb-5">
+            <div class="col-lg-4 offset-lg-4 bg-light roundend" id="caixaCadastro">
+                <h2 class="text-center">Cadastro de  usuario</h2>
+                     <form action="#" class="p-2" id="frmCadastro">
+                <div class="form-group">
+                <input type="text" name"nomeCompleto" id="nomeCompleto" class="form-control"
+                placeholder="nome completo" required>
+                </div>
 
+                <div class="form-group">
+                <input type="text" name="nomeUsuario" id="nomeUsuario" class="form-control" placeholder="nome de Usuario" required>
+                </div>
+
+                <div class="form-group">
+                <input type="email" name="emailUsuario" id="emailUsuario" class="form-control" placeholder="E-mail de Usuario" required>
+                </div>
+
+                <div class="form-group">
+                <input type="password" name="SenhaUsuario" id="SenhaUsuario"
+                class="form-control" placeholder="Digite sua senha" required>
+                </div>
+
+                <div class="form-group">
+                  <input type="password" name="SenhaConfirma" id="SenhaConfirma"
+                class="form-control" placeholder="Confirme sua senha" required></div>
+
+                <div class="form-group">
+                <div class="custom-control custom-checkbox">
+                <input type="checkbox" name="concordadar" id="concordar" class="custom-control-input">
+                <label for="concordar" class="custom-control-label">
+                Eu concordo com os <a href="#"> termos e condicões.</a>
+                </label>
+                
+                </div>
+                </div>
+
+                <div class="form-group">
+                <input type="submit" value=":: Registrar ::"
+                class="btn btn-primary btn-block" id="btnRegistrar">
+                </div>
+                
+                <div class="form-group">
+                <p class="text-center">ja registrado 
+                <a href="#" id="btnjaregistrado">
+                entrar aqui
+                </a>
+                
+                </p>
+                </div>
+                
+    
+    
+    </form>
+    
+    
+    </div>
+    
+    
+    
+    </section>
+
+    
+
+  
 </main>
     
 
