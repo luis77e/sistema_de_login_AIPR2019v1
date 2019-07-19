9cfc5b4d0a9fce26e6dbf758866ed48da8b283ef
luis <48030756+luis77e@users.noreply.github.com>
2019-07-19 16:36:43 -0300

    close #3
    Formulário de Recuperação De Senha

--- index.php
+++ index.php
@@ -7,6 +7,13 @@
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.min.css">
 <title>Sistema de login TNX Systems</title>
+<style>
+#caixaCadastro,
+#caixaRecuperarSenha,
+#alerta{
+      display: none;
+}
+</style>
 </head>
 <body class="bg-dark">
 <main class="container mt-4">
@@ -94,29 +101,27 @@
                   <input type="password" name="SenhaConfirma" id="SenhaConfirma"
                 class="form-control" placeholder="Confirme sua senha" required></div>
 
-                <div class="form-group">
-                <div class="custom-control custom-checkbox">
-                <input type="checkbox" name="concordadar" id="concordar" class="custom-control-input">
-                <label for="concordar" class="custom-control-label">
-                Eu concordo com os <a href="#"> termos e condicões.</a>
-                </label>
+                                    <div class="form-group">
+                                 <div class="custom-control custom-checkbox">
+                             <input type="checkbox" name="concordadar" id="concordar" class="custom-control-input">
+                         <label for="concordar" class="custom-control-label">
+                     Eu concordo com os <a href="#"> termos e condicões.</a>
+                    </label>
                 
-                </div>
-                </div>
+                 </div>
+                    </div>
 
-                <div class="form-group">
-                <input type="submit" value=":: Registrar ::"
-                class="btn btn-primary btn-block" id="btnRegistrar">
-                </div>
+                    <div class="form-group">
+                     <input type="submit" value=":: Registrar ::"
+                         class="btn btn-primary btn-block" id="btnRegistrar">
+                             </div>
                 
-                <div class="form-group">
-                <p class="text-center">ja registrado 
-                <a href="#" id="btnjaregistrado">
-                entrar aqui
-                </a>
-                
-                </p>
-                </div>
+                 <div class="form-group">
+                    <p class="text-center">ja registrado 
+                 <a href="#" id="btnjaregistrado">
+                    entrar aqui</a>
+                 </p>
+                     </div>
                 
     
     
@@ -127,6 +132,29 @@
     
     
     
+ </section>
+    <section class="row mb-5">
+        <div class="col-lg-4  offset-lg-4 bg-light rounded" id="caixaRecuperarSenha">
+             <h2 class="text-center">Gerar nova Senha</h2>
+                <form action="#" id="formSenha">
+                 <div class="form-group">
+                    <small class="text-muted" >
+                    Para gerar uma nova senha, digite seu e-mail e receba as instruções 
+                    </small>
+                 </div>
+             <div class="form-group">
+             <input type="email" name="emailSenha" id="emailSenha" class="form-control" placeholder="E-mail" required>
+             </div>
+         <div class="form-group">
+         <input type="submit" value=":: Instruções ::" id="btnEnviarEmail" class="btn btn-primary btn-block">
+         </div>
+     <div class="form-group float-right">
+         <a href="#" id="btnVoltar">Voltar</a>
+     </div>    
+    </form>
+
+    </div>
+
     </section>
 
     
@@ -138,5 +166,29 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.slim.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.15.0/umd/popper.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+<script>
+/* jQuery */
+$(function(){
+    $("#btnEsqueci").click(function(){
+        $("#caixaLogin").hide();
+        $("#caixaRecuperarSenha").show();
+    });
+    //Voltar para a tela de login
+    $("#btnVoltar").click(function(){
+        $("#caixaRecuperarSenha").hide();
+        $("#caixaLogin").show();
+    })
+    //Trocar de tela de login para cadastro de usuario 
+    $("#btnCadastrar").click(function(){
+        $("#caixaLogin").hide();
+        $("#caixaCadastro").show();
+    })
+    //Voltar para a tabela de login 
+    $("#btnJaCadastrado").click(function(){
+        $("#caixaLogin").show();
+        $("#caixaCadastro").hide();
+    })
+});
+</script>
 </body>
 </html>
