0073750c33ca914e6e9323fe0081d01a048d87a6
Luis <luis_mannrich@estudante.sc.senai>
2019-08-30 16:31:49 -0300

    Cadastro de usuario
    closes #8

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 # sistema_de_login_AIPR2019v1
 Sistema de Login com registro de novo usuário e recuperação de senha
 
-
+## aula06 - 30/08/19
+cadastro de usuarios 
 ## aula05 - 23/08/19
-Criação do banco de dados e ~cadastro de usuarios 
+Criação do banco de dados e 
 ## aula04 - 15/08/19
 Recepção de dados no back-end
 
--- recebe.php
+++ recebe.php
@@ -1,51 +1,62 @@
 <?php
-//é necessario fazer a conexão com o banco de dados 
-require_once "configDB.php";
-  
 
-function verificar_entrada($entrada){
-    $saida = trim($entrada);// Remove espaços 
-    $saida = stripslashes($saida);
+// É necessário fazer a conexão com o Banco de Dados
+require_once "configDB.php";
+
+
+function verificar_entrada($entrada)
+{
+    $saida = trim($entrada); //Remove espaços antes e depois
+    $saida = stripslashes($saida); //Remove as barras
     $saida = htmlspecialchars($saida);
     return $saida;
 }
 
-if(isset($_POST['action']) && $_POST['action'] == 'cadastro'){
-     
-   $nomeCompleto = verificar_entrada($_POST['nomeCompleto']);
-   $nomeUsuario = verificar_entrada($_POST['nomeUsuário']);
-   $emailUsuario = verificar_entrada($_POST['emailUsuário']);
-   $senhaUsuario = verificar_entrada($_POST['senhaUsuário']);
-   $senhaConfirma = verificar_entrada($_POST['senhaConfirma']);
-   $concordar = $_POST['concordar'];
-   $dataCriacao = date("y-m-d H:i:s");
-   //Hash da senha / Codificação de senha em 40 caracteres
-   $senha = sha1($senhaUsuario);
-   $senhaC = sha1($senhaConfirma);
+
+if (
+    isset($_POST['action']) &&
+    $_POST['action'] == 'cadastro'
+) {
+    //Pegar os campos do formulário
+    $nomeCompleto = verificar_entrada($_POST['nomeCompleto']);
+    $nomeUsuario = verificar_entrada($_POST['nomeUsuário']);
+    $emailUsuario = verificar_entrada($_POST['emailUsuário']);
+    $senhaUsuario = verificar_entrada($_POST['senhaUsuário']);
+    $senhaConfirma = verificar_entrada($_POST['senhaConfirma']);
+    $concordar = $_POST['concordar'];
+    $dataCriacao = date("Y-m-d H:i:s");
+    //Hash de senha / Codificação de senha em 40 caracteres
+    $senha = sha1($senhaUsuario);
+    $senhaC = sha1($senhaConfirma);
 
 
-
-   echo"<h5>nome Completo: $nomeCompleto</h5>";
-
-   echo"<h5>nome Usuário: $nomeUsuario</h5>";
-
-    echo"<h5>E-mail Usuário: $emailUsuario</h5>";
-
-   echo "<h5>Senha Usuário: $senhaUsuario</h5>";
-
-   echo "<h5>Senha Confirma: $SenhaConfirma</h5>";
-
-   echo "<h5>Concordar: $concordar</h5>";
-
-   echo "<h5>Data de Criação: $dataCriacao</h5>";
-
-   if($senha != $senhaC){
-       echo "<h1>as senhas não conferem</h1>";
-       exit();
-
-   }else{
-       echo "<h5> senha codificada: $senha</h5>";
-   }
-}else{
-    echo "<h1 style='color:red'>Esta Pagina Não pode ser acessada diretamente</h1>";
+    if ($senha != $senhaC) {
+        echo "<h1>As senhas não conferem</h1>";
+        exit();
+    } else {
+        //echo "<h5> senha codificada: $senha</h5>";
+        //Verificar se o usuário já existe no banco de dados
+        $sql = $conecta->prepare("SELECT nomeUsuario, email FROM usuario WHERE nomeUsuario = ? OR email = ?");
+        //Substitui cada ? por uma string abaixo
+        $sql->bind_param("ss", $nomeUsuario, $emailUsuario);
+        $sql->execute();
+        $resultado = $sql->get_result();
+        $linha = $resultado->fetch_array(MYSQLI_ASSOC);
+        if ($linha['nomeUsuario'] == $nomeUsuario) {
+            echo "<p>Nome de usuário indisponível, tente outro</p>";
+        } elseif ($linha['email'] == $emailUsuario) {
+            echo "<p>E-mail já em uso, tente outro</p>";
+        } else {
+            $sql = $conecta->prepare("INSERT into usuario (nome, nomeUsuario, email, senha, dataCriacao) values(?, ?, ?, ?, ?)");
+            $sql->bind_param("sssss", $nomeCompleto, $nomeUsuario, $emailUsuario, $senha, $dataCriacao);
+            if ($sql->execute()) {
+                echo "<p>Registrado com sucesso</p>";
+            } else {
+                echo "<p>Algo deu errado. Tente outra vez.</p>";
+            }
+        }
+    }
+} else {
+    echo "<h1 style='color:red'>Esta página não pode 
+    ser acessada diretamente</h1>";
 }
